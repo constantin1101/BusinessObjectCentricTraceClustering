ae8a971d60603bc0efddd4c2563999c331047031
constantin1101 <68335602+constantin1101@users.noreply.github.com>
2021-09-20 14:54:41 +0200

    Delete alpha_miner.py

--- alpha_miner.py
+++
@@ -1,5 +0,0 @@
-import pm4py
-from pm4py.algo.discovery.alpha import algorithm as alpha_miner
-
-log = pm4py.read_xes("input/logs/HB_extract.xes")
-net, initial_marking, final_marking = alpha_miner.apply(log)
